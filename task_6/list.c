80e48e4dc64bf086dac0db9d85f3f117cb09f017
Nikolai Popov <nkl.popov@yandex.ru>
2024-01-08 22:50:20 +0700

    task_6

---
+++ task_6/list.c
@@ -0,0 +1,100 @@
+#include "task_6.h"
+
+list *addElem(list *head, phoneBook a) {
+    list *newElem = (list*)malloc(sizeof(list));
+    list *tmp = head;
+    newElem->profile = a;
+    if(NULL == head) {
+        newElem->prev = newElem;
+        newElem->next = newElem;
+        head = newElem;
+        return head;
+    }
+    do {
+        if(newElem->profile.id <= tmp->profile.id) {
+            newElem->next = tmp;
+            newElem->prev = tmp->prev;
+            tmp->prev->next = newElem;
+            tmp->prev = newElem;
+            if(tmp == head) head = newElem;
+            return head;
+        }
+        tmp = tmp->next;
+    } while(tmp != head);
+    newElem->next = tmp;
+    newElem->prev = tmp->prev;
+    tmp->prev->next = newElem;
+    tmp->prev = newElem;
+    return head;
+}
+
+list *deleteElem(list *head, int val) {
+    list *tmp = head;
+    if(NULL == head) {
+        printw("List empty!\n");
+        return head;
+    }
+    do {
+        if(val == tmp->profile.id) {
+            if(tmp->next == tmp->prev) {
+                free(tmp);
+                printw("Single profile is deleted\n");
+                return NULL;
+            }
+            tmp->next->prev = tmp->prev;
+            tmp->prev->next = tmp->next;
+            if(tmp == head) head = head->next;
+            free(tmp);
+            printw("Profile ID=%d is deleted!\n", val);
+            return head;
+        }
+        tmp = tmp->next;
+    } while(tmp != head);
+    printw("List does not contain %d!\n", val);
+    return head;
+}
+
+list *deleteList(list *head) {
+    list *tmp = head;
+    list *delete;
+    if(NULL == head) {
+        return NULL;
+    }
+    do {
+        delete = tmp;
+        tmp = tmp->next;
+        free(delete);
+    } while(tmp != head);
+    return NULL;
+}
+
+list *findInList(list* head, int id) {
+    list *tmp = head;
+    if(NULL == head) {
+        printw("List empty!\n");
+        return head;
+    }
+    do {
+        if(tmp->profile.id == id)
+            return tmp;
+        tmp = tmp->next;
+    } while(tmp != head);
+    if (tmp == head && tmp->profile.id != id) {
+        printw("Wrong id\n");
+        return NULL;
+    }
+    return tmp;
+}
+
+void printList(list *head) {
+    list *tmp = head;
+    if(NULL == head) {
+        printw("List empty!\n");
+        return;
+    }
+    do {
+        printProfile(tmp->profile);
+        tmp = tmp->next;
+    } while(tmp != head);
+    printw("\n");
+}
---
+++ task_6/task_6.h
@@ -0,0 +1,42 @@
+#ifndef TASK_6
+#define TASK_6
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <time.h>
+#define N 20
+#define cntSocial 2
+
+typedef struct phoneBook {
+    int id;
+    char lastName[N];
+    char firstName[N];
+    char fatherName[N];
+    char job[N];
+    char jobTitle[N];
+    long int phone;
+    char email[N];
+    char socialLinks[cntSocial][N];
+} phoneBook;
+
+typedef struct list {
+    phoneBook profile;
+    struct list *next;
+    struct list *prev;
+} list;
+
+list *addElem(list *head, phoneBook a);
+list *deleteElem(list *head, int val);
+list *deleteList(list *head);
+list *findInList(list* head, int id);
+void printList(list *head);
+
+void showMenu();
+list *addProfile(list *head);
+void printProfile(phoneBook profile);
+void deleteProfile(phoneBook *profile);
+list *editProfile(list *head, int id);
+
+#endif
